3a0b270e233f1b64b1ec30cb30f1916f7f499c3b
jerso <jersoroka@gmail.com>
2021-11-02 20:13:50 -0700

    Add option menu and options

--- src/components/menus/Option.js
+++ src/components/menus/Option.js
@@ -4,7 +4,7 @@ import "./Option.css"
 export const Option = ({icon, text}) => {
     return (
         <div className="option__container">
-            <span>{icon}</span>
+            <span className="option__icon-container">{icon}</span>
             <span className="option__text">{text}</span>
         </div>
     )
--- src/components/menus/OptionsMenu.js
+++ src/components/menus/OptionsMenu.js
@@ -4,9 +4,13 @@ import "./OptionsMenu.css"
 import "./Option.css"
 import { Option } from './Option';
 import { FaUserTimes } from 'react-icons/fa'
+import { MdPostAdd } from 'react-icons/md';
+import { BiBlock, BiVolumeMute } from 'react-icons/bi';
+import { ImEmbed } from 'react-icons/im';
+import { IoFlagOutline } from 'react-icons/io5';
 
 export const OptionsMenu = ({ setIsOptionsMenuOpen }) => {
-    const { useClickOutside } = useContext(GlobalContext);
+    const { handle, useClickOutside } = useContext(GlobalContext);
 
     let domNode = useClickOutside(() => {
         setIsOptionsMenuOpen(false);
@@ -14,7 +18,12 @@ export const OptionsMenu = ({ setIsOptionsMenuOpen }) => {
 
     return (
         <div className="optionsMenu__container" ref={domNode}>
-            <Option icon={<FaUserTimes className="option__icon"/>} text={"Hello World"}/>
+            <Option icon={<FaUserTimes/>} text={"Unfollow " + handle}/>
+            <Option icon={<MdPostAdd/>} text={"Add/remove " + handle + " from Lists"}/>
+            <Option icon={<BiVolumeMute/>} text={"Mute " + handle}/>
+            <Option icon={<BiBlock/>} text={"Block " + handle}/>
+            <Option icon={<ImEmbed/>} text={"Embed Tweet"}/>
+            <Option icon={<IoFlagOutline/>} text={"Report Tweet"}/>
         </div>
     )
 }
